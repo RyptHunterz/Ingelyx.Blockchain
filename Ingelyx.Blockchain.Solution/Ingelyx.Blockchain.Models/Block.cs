bda058e5c15f0434c24b2099d1c7f51107d947a1
Stéphane Ducoroy <stephane@ducoroy.be>
2019-06-30 14:53:45 +0200

    Add Model Block & Transaction / Create Helper SHA256

---
+++ Ingelyx.Blockchain.Solution/Ingelyx.Blockchain.Models/Block.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ingelyx.Blockchain.Commons.Helper;
+
+namespace Ingelyx.Blockchain.Models
+{
+    public class Block
+    {
+        public Guid Index { get; set; }
+        public string PreviousHash { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<Transaction> Data { get; set; }
+        public string Hash { get; set; }
+
+        public Block(Guid index, string previousHash, DateTime timestamp, List<Transaction> data, string hash)
+        {
+            this.Index = index;
+            this.PreviousHash = previousHash;
+            this.Timestamp = timestamp;
+            this.Data = data;
+            this.Hash = hash;
+        }
+
+        public string calculateHash(Guid index, string previousHash, DateTime timestamp, List<Transaction> transactionList)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(var item in transactionList)
+            {
+                stringBuilder.Append(item);
+            }
+
+            return SHA256Helper.Sha256Hash(index + previousHash + timestamp + stringBuilder);
+        }
+    }
+}
---
+++ Ingelyx.Blockchain.Solution/Ingelyx.Blockchain.Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Ingelyx.Blockchain.Models
+{
+    public class Transaction
+    {
+        public string idColumn { get; set; }
+        public string idLigne { get; set; }
+        public string methodAccess { get; set; } // PUT / DELETE / POST
+        public string data { get; set; }
+
+        public string rawData
+        { 
+            get
+            {
+                return string.Format("/{0};{1};{2};{3}", idColumn, idLigne, methodAccess, data);
+            }
+        }
+    }
+}
